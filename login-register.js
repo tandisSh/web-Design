63943c39a6eb792f2342722ca9b4822710d74bae
tandisSh <tandis00shojaee@gmail.com>
2023-12-15 01:07:51 +0330

    login/register

---
+++ login-register.js
@@ -0,0 +1,19 @@
+function signup()
+{
+    document.querySelector(".login-form-container").style.cssText = "display: none;";
+    document.querySelector(".signup-form-container").style.cssText = "display: block;";
+    document.querySelector(".container").style.cssText = " background: linear-gradient(to bottom, rgb(6, 108, 100),  rgb(14, 48, 122));";
+    document.querySelector(".button-1").style.cssText = "display: none";
+    document.querySelector(".button-2").style.cssText = "display: block";
+
+};
+
+function login()
+{
+    document.querySelector(".signup-form-container").style.cssText = "display: none;";
+    document.querySelector(".login-form-container").style.cssText = "display: block;";
+    document.querySelector(".container").style.cssText = " background: linear-gradient(to bottom, rgb(6, 108, 100),  rgb(14, 48, 122));";
+    document.querySelector(".button-2").style.cssText = "display: none";
+    document.querySelector(".button-1").style.cssText = "display: block";
+
+}
