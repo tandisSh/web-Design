a38d401ca4a53b0c37e1bf9ce8d05beb8a90cd66
tandisSh <tandis00shojaee@gmail.com>
2023-12-15 11:44:42 +0330

    add icon

--- login-register.js
+++ login-register.js
@@ -2,7 +2,7 @@ function signup()
 {
     document.querySelector(".login-form-container").style.cssText = "display: none;";
     document.querySelector(".signup-form-container").style.cssText = "display: block;";
-    document.querySelector(".container").style.cssText = " background: linear-gradient(to bottom, rgb(6, 108, 100),  rgb(14, 48, 122));";
+    document.querySelector(".container").style.cssText = "linear-gradient(to bottom, rgba(193, 44, 230, 0.882),  rgb(225, 230, 90));";
     document.querySelector(".button-1").style.cssText = "display: none";
     document.querySelector(".button-2").style.cssText = "display: block";
 
@@ -12,8 +12,32 @@ function login()
 {
     document.querySelector(".signup-form-container").style.cssText = "display: none;";
     document.querySelector(".login-form-container").style.cssText = "display: block;";
-    document.querySelector(".container").style.cssText = " background: linear-gradient(to bottom, rgb(6, 108, 100),  rgb(14, 48, 122));";
+    document.querySelector(".container").style.cssText = " linear-gradient(to bottom, rgba(193, 44, 230, 0.882),  rgb(225, 230, 90));";
     document.querySelector(".button-2").style.cssText = "display: none";
     document.querySelector(".button-1").style.cssText = "display: block";
 
 }
+
+// JavaScript code (can be placed in login-register.js file)
+
+window.addEventListener("DOMContentLoaded", function() {
+  const inputFields = document.querySelectorAll(".input-field");
+
+  inputFields.forEach(function(input) {
+      input.addEventListener("focus", function() {
+          this.previousElementSibling.style.top = "-15px";
+          this.previousElementSibling.style.left = "10px";
+          this.previousElementSibling.style.opacity = "1";
+          this.previousElementSibling.style.color = "#333333";
+      });
+
+      input.addEventListener("blur", function() {
+          if (this.value === "") {
+              this.previousElementSibling.style.top = "0";
+              this.previousElementSibling.style.left = "10px";
+              this.previousElementSibling.style.opacity = "1";
+              this.previousElementSibling.style.color = "#666666";
+          }
+      });
+  });
+});
